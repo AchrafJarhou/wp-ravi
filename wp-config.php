021667fc79b69c8e829918469e5f983b64abbc46
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2026-09-14 20:51:43 +0200

    Fix: enlever exit(0) qui peut causer les erreurs 500

--- wp-config.php
+++ wp-config.php
@@ -121,11 +121,6 @@ if (in_array($origin, $allowed_origins, true)) {
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, PATCH');
     header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
     header('Access-Control-Allow-Credentials: true');
-
-    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-        http_response_code(200);
-        exit(0);
-    }
 }
 
 /** Absolute path to the WordPress directory. */
