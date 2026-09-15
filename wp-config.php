bb11408e21d8b641a8f6656b8821387a83aba0d0
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2026-09-15 15:20:22 +0200

    Fix: ajouter domaines Vercel avec et sans slash à la liste CORS

--- wp-config.php
+++ wp-config.php
@@ -115,7 +115,9 @@ $allowed_origins = [
     'http://127.0.0.1:5173',
     'http://10.60.4.51:5173',
     'https://template-woo-commerce-headless.vercel.app',
+    'https://template-woo-commerce-headless.vercel.app/',
     'https://template-woo-commerce-headless-3jh7wv8rz.vercel.app',
+    'https://template-woo-commerce-headless-3jh7wv8rz.vercel.app/',
 ];
 
 $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
